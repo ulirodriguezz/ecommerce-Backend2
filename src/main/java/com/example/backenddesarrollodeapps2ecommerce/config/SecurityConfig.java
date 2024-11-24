c3857e49c9d2d8e55a59f1d08188f38bee0f982c
ulirodriguezz <ulirodrigueze@gmail.com>
2024-11-24 16:15:57 -0300

    Fixes al CORS e implentacion del S3 comentada

--- src/main/java/com/example/backenddesarrollodeapps2ecommerce/config/SecurityConfig.java
+++ src/main/java/com/example/backenddesarrollodeapps2ecommerce/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+		//
 	}
 
 	@Bean
